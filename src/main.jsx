4e7c615d6a528b39146c002846041ad6c70bfe04
handikatriarlan <handikaarlan@gmail.com>
2025-03-14 11:22:22 +0700

    feat(routes): implement basic routing and views

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from "./context/AuthContext"
-import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
---
+++ src/routes/index.jsx
@@ -0,0 +1,38 @@
+import { useContext } from "react"
+import { AuthContext } from "../context/AuthContext"
+import { Routes, Route, Navigate } from "react-router-dom"
+import Home from "../views/home/index.jsx"
+import Register from "../views/auth/register.jsx"
+import Login from "../views/auth/login.jsx"
+
+export default function AppRoutes() {
+  const { isAuthenticated } = useContext(AuthContext)
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route
+        path="/register"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/admin/dashboard" replace />
+          ) : (
+            <Register />
+          )
+        }
+      />
+
+      <Route
+        path="/login"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/admin/dashboard" replace />
+          ) : (
+            <Login />
+          )
+        }
+      />
+    </Routes>
+  )
+}
