0587197d92f8a21185c110f509fddb00e7c87049
handikatriarlan <handikaarlan@gmail.com>
2025-03-14 11:38:18 +0700

    feat(admin): add user routes and views

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,6 +5,9 @@ import Home from "../views/home/index.jsx"
 import Register from "../views/auth/register.jsx"
 import Login from "../views/auth/login.jsx"
 import Dashboard from "../views/admin/dashboard/index.jsx"
+import UsersIndex from "../views/admin/users/index.jsx"
+import UsersCreate from "../views/admin/users/create.jsx"
+import UsersEdit from "../views/admin/users/edit.jsx"
 
 export default function AppRoutes() {
   const { isAuthenticated } = useContext(AuthContext)
@@ -39,6 +42,27 @@ export default function AppRoutes() {
         path="/admin/dashboard"
         element={
           isAuthenticated ? <Dashboard /> : <Navigate to="/login" replace />
+        }
+      />
+
+      <Route
+        path="/admin/users"
+        element={
+          isAuthenticated ? <UsersIndex /> : <Navigate to="/login" replace />
+        }
+      />
+
+      <Route
+        path="/admin/users/create"
+        element={
+          isAuthenticated ? <UsersCreate /> : <Navigate to="/login" replace />
+        }
+      />
+
+      <Route
+        path="/admin/users/edit/:id"
+        element={
+          isAuthenticated ? <UsersEdit /> : <Navigate to="/login" replace />
         }
       />
     </Routes>
