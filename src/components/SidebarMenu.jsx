8faaa2bdb6859b20e4a1e908de65464fc8769232
handikatriarlan <handikaarlan@gmail.com>
2025-03-14 11:30:48 +0700

    feat(auth): add logout functionality to sidebar menu

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,6 +1,22 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useContext } from "react"
+import Cookies from "js-cookie"
+import { AuthContext } from "../context/AuthContext"
 
 export default function SidebarMenu() {
+  const navigate = useNavigate()
+
+  const { setIsAuthenticated } = useContext(AuthContext)
+
+  const logout = () => {
+    Cookies.remove("token")
+    Cookies.remove("user")
+
+    setIsAuthenticated(false)
+
+    navigate("/login", { replace: true })
+  }
+
   return (
     <div className="card border-0 rounded shadow-sm">
       <div className="card-header">MAIN MENU</div>
@@ -20,7 +36,7 @@ export default function SidebarMenu() {
             Users
           </Link>
           <a
-            href="#"
+            onClick={logout}
             className="list-group-item list-group-item-action"
             style={{ cursor: "pointer" }}
           >
